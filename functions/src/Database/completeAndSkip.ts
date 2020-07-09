60f8222402909f5e0ed98dc8c6b52b2de79a2c98
Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
2020-07-09 19:06:23 -0400

    BREAKING CHANGE(functions): Added Stubbed Out Function Bodies (#5)
    * chore(functions): Connected Google Firestore Database
    
    * chore(functions): Added Error Handler Helper Methods
    
    * BREAKING CHANGE(functions): Added Stubbed Out Function Bodies
    
    Note: The functions return a promise. That's how they work... but that was not accounted for in the stubbed out data types.

--- functions/src/Database/completeAndSkip.ts
+++ functions/src/Database/completeAndSkip.ts
@@ -1,5 +1,66 @@
-import { FoodObject } from "./foodsAndCategories";
+import { FoodObject, getRandomItem, categories } from "./foodsAndCategories";
+import { foodRef } from "../config/firebaseConfig";
+import { logReturnFalse, logReturnNull } from "../helpers/errorHelpers";
 
-export const complete = (category: string) => true;
+export const complete = async (category: string) => {
+  return foodRef
+    .get()
+    .then((doc) => {
+      const data = doc.data();
+      if (data) {
+        const list: FoodObject[] = data.list;
+        for (let i = 0; i < list.length; i++) {
+          if (list[i].category.toLowerCase().includes(category.toLowerCase())) {
+            list.splice(i, 1);
+            break;
+          }
+        }
+        return foodRef
+          .update({ list })
+          .then(() => true)
+          .catch(logReturnFalse);
+      } else {
+        console.log("No data!");
+        return false;
+      }
+    })
+    .catch(logReturnFalse);
+};
 
-export const skip = (category: string) => [] as FoodObject[];
+export const skip = async (category: string) => {
+  return foodRef
+    .get()
+    .then((doc) => {
+      const data = doc.data();
+      if (data) {
+        const list: FoodObject[] = data.list;
+        for (let i = 0; i < list.length; i++) {
+          if (list[i].category.toLowerCase().includes(category.toLowerCase())) {
+            list.splice(i, 1);
+            break;
+          }
+        }
+        let catIndex = 0;
+        for (; catIndex < categories.length; catIndex++) {
+          if (
+            categories[catIndex].toLowerCase().includes(category.toLowerCase())
+          ) {
+            break;
+          }
+        }
+        const newFoodItem: FoodObject = {
+          label: getRandomItem(category),
+          category: categories[catIndex],
+        };
+        list.push(newFoodItem);
+        return foodRef
+          .update({ list })
+          .then(() => newFoodItem)
+          .catch(logReturnNull);
+      } else {
+        console.log("No data!");
+        return null;
+      }
+    })
+    .catch(logReturnNull);
+};
--- functions/src/Database/newDayAndGetData.ts
+++ functions/src/Database/newDayAndGetData.ts
@@ -1,5 +1,44 @@
-import { FoodObject } from "./foodsAndCategories";
+import { FoodObject, categories, getRandomItem } from "./foodsAndCategories";
+import { foodRef } from "../config/firebaseConfig";
+import { logReturnEmptyArray } from "../helpers/errorHelpers";
 
-export const newDay = () => [] as FoodObject[];
+export const newDay = async (): Promise<FoodObject[] | null> => {
+  return foodRef
+    .get()
+    .then((doc) => {
+      const data = doc.data();
+      if (data) {
+        const list: FoodObject[] = data.list;
+        for (const category of categories) {
+          let exists = false;
+          for (const item of list) {
+            if (item.category.includes(category)) {
+              exists = true;
+              break;
+            }
+          }
+          if (!exists) {
+            list.push({ label: getRandomItem(category), category: category });
+          }
+        }
+        return foodRef
+          .update({ list })
+          .then(() => list)
+          .catch(logReturnEmptyArray);
+      } else {
+        console.log("No data!");
+        return [];
+      }
+    })
+    .catch(logReturnEmptyArray);
+};
 
-export const getFoods = () => [] as FoodObject[];
+export const getFoods = async (): Promise<FoodObject[] | null> => {
+  return foodRef
+    .get()
+    .then((doc) => {
+      const data = doc.data();
+      return data ? (data.list as FoodObject[]) : null;
+    })
+    .catch(logReturnEmptyArray);
+};
