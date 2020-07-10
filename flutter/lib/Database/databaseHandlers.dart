a810a1d1967a6acb2ee5607cb0e00877dabe9e48
Andrew Dimmer <contactandrewd@gmail.com>
2020-07-10 02:13:14 -0400

    feat(flutter): Added Database Handlers to Get, Complete, and Skip Foods

---
+++ flutter/lib/Database/databaseHandlers.dart
@@ -0,0 +1,94 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:immunity_booster/Classes/FoodObject.dart';
+
+Future<bool> completeDatabaseUpate(String category) async {
+  // NOTE: Due to Google Cloud Platform Billing Restrictions, we were unable to create a new project for this hack.
+  // To fix this, we deployed our fuctions to an archived project still connected to billing to run cloud functions.
+  final response = await http.post(
+      'https://us-central1-hackcation2020-gcp.cloudfunctions.net/complete_flutter',
+      body: category);
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return json.decode(response.body) as bool;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    print(response.statusCode);
+    print(response.reasonPhrase);
+    print(response.body);
+    throw Exception('Failed to mark food as complete.');
+  }
+}
+
+Future<FoodObject> skipDatabaseUpdate(String category) async {
+  // NOTE: Due to Google Cloud Platform Billing Restrictions, we were unable to create a new project for this hack.
+  // To fix this, we deployed our fuctions to an archived project still connected to billing to run cloud functions.
+  final response = await http.post(
+      'https://us-central1-hackcation2020-gcp.cloudfunctions.net/skip_flutter',
+      body: category);
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return FoodObject.fromJSON(json.decode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    print(response.statusCode);
+    print(response.reasonPhrase);
+    print(response.body);
+    throw Exception('Failed skip food.');
+  }
+}
+
+Future<List<FoodObject>> newDayDatabaseUpdate() async {
+  // NOTE: Due to Google Cloud Platform Billing Restrictions, we were unable to create a new project for this hack.
+  // To fix this, we deployed our fuctions to an archived project still connected to billing to run cloud functions.
+  final response = await http.post(
+    'https://us-central1-hackcation2020-gcp.cloudfunctions.net/new_day_flutter',
+  );
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    List<dynamic> list = json.decode(response.body)["list"];
+    return list.map((food) {
+      return FoodObject.fromJSON(json.decode(json.encode(food)));
+    }).toList();
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    print(response.statusCode);
+    print(response.reasonPhrase);
+    print(response.body);
+    throw Exception('Failed to reset the list for a new day.');
+  }
+}
+
+Future<List<FoodObject>> getFoodsDatabaseCall() async {
+  // NOTE: Due to Google Cloud Platform Billing Restrictions, we were unable to create a new project for this hack.
+  // To fix this, we deployed our fuctions to an archived project still connected to billing to run cloud functions.
+  final response = await http.post(
+    'https://us-central1-hackcation2020-gcp.cloudfunctions.net/get_foods_flutter',
+  );
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    List<dynamic> list = json.decode(response.body)["list"];
+    return list.map((food) {
+      return FoodObject.fromJSON(json.decode(json.encode(food)));
+    }).toList();
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    print(response.statusCode);
+    print(response.reasonPhrase);
+    print(response.body);
+    throw Exception('Failed to get foods.');
+  }
+}
